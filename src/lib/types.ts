ae9dcab1f18d98a29bdd64bab257dfb1a93b8cf1
nearHaan <farhaan.nizam675@gmail.com>
2025-11-16 00:11:52 +0530

    add loadedData | DetailsTemplateType

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,6 +8,33 @@ export type NavMenuItem = {
 
 export type TemplateSubComponent = {
 	title: string;
-	type: 'photo' | 'text' | 'number' | 'email' | 'url' | 'text-area' | 'date' | 'skill';
+	type: 'photo' | 'text' | 'number' | 'email' | 'url' | 'text-area' | 'date' | 'skill' | string;
 	example: string;
 };
+
+export type LoadedData<T> = LoadingSuccess<T> | LoadingPending | LoadingFailure;
+
+type LoadingPending = {
+	state: 'pending';
+	message: string;
+};
+
+type LoadingSuccess<T> = {
+	state: 'success';
+	data: T;
+};
+
+type LoadingFailure = {
+	state: 'failed';
+	message: string;
+};
+
+type DetailsTemplateItem = {
+	meta: {
+		desc: string;
+		type: 'single' | 'list';
+	};
+	subComponents: { [key: string]: { type: string; example: string } };
+};
+
+export type DetailsTemplateType = { [key: string]: DetailsTemplateItem };
