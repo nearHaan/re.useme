ba7ee6ed73806570e4f5fb5362e76ea01dd69831
nearHaan <farhaan.nizam675@gmail.com>
2025-11-16 00:07:29 +0530

    first commit

---
+++ src/lib/components/bio-section-btn.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	const {
+		title,
+		onClick,
+		isActive
+	}: {
+		title: string;
+		onClick: Function;
+		isActive: boolean;
+	} = $props();
+</script>
+
+<button
+	onclick={() => {
+		onClick(title);
+	}}
+	class="w-full rounded bg-background p-sm text-left"
+>
+	<p class="font-semibold">{title}</p>
+</button>
