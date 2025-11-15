706e30c116fc8ae6e4a4e942240dbd0a7f43be2f
nearHaan <farhaan.nizam675@gmail.com>
2025-11-15 10:35:52 +0530

    add mobile view menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { BookUserIcon, FileIcon, LayoutDashboardIcon } from 'lucide-svelte';
+	import { BookUserIcon, FileIcon, LayoutDashboardIcon, MenuIcon } from 'lucide-svelte';
 	import SidebarBtn from '$lib/components/sidebar-btn.svelte';
 	import type { NavMenuItem } from '$lib/types';
 	import { goto } from '$app/navigation';
 
 	let { children } = $props();
 	let activeTab = $state('Resumes');
+	let meunOpen = $state(false);
 
 	const menuItems: NavMenuItem[] = [
 		{
@@ -27,6 +28,14 @@
 		}
 	];
 
+	function toggleMenu() {
+		meunOpen = !meunOpen;
+	}
+
+	function closeMenu() {
+		meunOpen = false;
+	}
+
 	function onTabClick(title: string, href: string) {
 		activeTab = title;
 		goto(href);
@@ -38,11 +47,41 @@
 </svelte:head>
 
 <div class="flex h-screen w-screen flex-col">
+	{#if meunOpen}
+		<div
+			class="absolute inset-0 bg-transparent-black"
+			tabindex="0"
+			onkeydown={(e) => {
+				if (e.key === 'Escape') {
+					closeMenu();
+				}
+			}}
+			role="button"
+			onclick={() => {
+				closeMenu();
+			}}
+		>
+			<div class="h-full w-60 bg-white p-sm">
+				{#each menuItems as item}
+					<SidebarBtn
+						Icon={item.icon}
+						title={item.title}
+						href={item.href}
+						onClick={onTabClick}
+						isActive={activeTab === item.title}
+					/>
+				{/each}
+			</div>
+		</div>
+	{/if}
 	<header class="flex h-15 w-full items-center justify-between bg-primary px-5">
 		<h2 class="text-white">re.useme</h2>
+		<button class="min-md:hidden" onclick={toggleMenu}>
+			<MenuIcon color="white" />
+		</button>
 	</header>
 	<div class="flex h-full">
-		<div class="w-60 p-sm">
+		<div class="w-60 p-sm max-md:hidden">
 			{#each menuItems as item}
 				<SidebarBtn
 					Icon={item.icon}
@@ -58,3 +97,5 @@
 		</div>
 	</div>
 </div>
+
+<svelte:window onkeydown={(e) => e.key === 'Escape' && closeMenu()} />
