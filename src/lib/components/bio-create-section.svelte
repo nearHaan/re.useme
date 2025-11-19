e4bdd0004456701546e64f7560a3c3104547fdea
nearHaan <farhaan.nizam675@gmail.com>
2025-11-19 17:48:53 +0530

    first commit

---
+++ src/lib/components/bio-create-section.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import CheckboxBtn from './checkbox-btn.svelte';
+
+	let {
+		title,
+		components,
+		selectedData = $bindable({})
+	}: { title: String; components: Array<object>; selectedData: object } = $props();
+</script>
+
+<div class="flex flex-col rounded-sm border-2 border-darkerBg">
+	<h3 class="bg-darkerBg p-xs">{title}</h3>
+	<div class="flex flex-col gap-y-xs p-xs">
+		{#each Object.entries(components) as [component, value]}
+			<div class="flex items-center rounded-sm bg-darkBg p-xs">
+				<div class="flex w-full flex-col">
+					<p class="text-xs">{component}</p>
+					<p>{value}</p>
+				</div>
+				<CheckboxBtn isChecked={false} />
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/lib/components/checkbox-btn.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { LucideCheck } from 'lucide-svelte';
+	let { isChecked = $bindable(false) }: { isChecked: Boolean } = $props();
+</script>
+
+<button
+	onclick={() => {
+		isChecked = !isChecked;
+	}}
+	class="flex h-5 w-5 items-center justify-center rounded-full border border-text bg-green-400 p-xxxs text-text"
+>
+	{#if isChecked}
+		<LucideCheck />
+	{/if}
+</button>
